802205609d428f1a85eef796102b31c76a89fb4d
Daniel de Oliveira <dann.ew@outlook.com>
2021-11-08 17:08:17 -0300

    ultimas mudanças

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
-const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
+// Controllers
 const gatosController = require("./gatos/GatosController");
 const usersController = require("./users/UsersController");
-
-const Gatos = require("./gatos/Gatos");
-const User = require("./users/User");
 
 // View engine
 app.set("view engine", "ejs");
